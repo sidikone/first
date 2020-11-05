37457d253dd4c8a8e4c79355624111d23ee87d82
sidikone <aboubakone@gmail.com>
2020-11-05 17:30:25 +0100

    cleaning

--- hello.py
+++ hello.py
@@ -2,15 +2,9 @@
 # --------------
 import numpy as np
 import pandas as pd
-<<<<<<< HEAD
-
-
-# Script writing
-# --------------
-=======
 import matplotlib.pyplot as plt
 
 print ("Hello world !")
 
 plt.plot(np.ones(5))
->>>>>>> plot_figure
+plot_figure
